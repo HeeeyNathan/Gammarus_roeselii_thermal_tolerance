bfebc1c11a54f69c45039f6cb351739b1e246dc6
HeeeyNathan <Nathan93Baker@gmail.com>
2022-10-11 17:55:32 +0300

    updated r scripts

--- Analysing survival data.R
+++ Analysing survival data.R
@@ -126,75 +126,27 @@ summary(cox_reg1)
 # the model with the motu covariate does not perform significantly better than a model with no covariates.
 
 # estimate cox proportional hazards (ph) model (cox regression model)
-cox_reg2 <- coxph(Surv(temp, surv) ~ motu + popf,
+cox_reg2 <- coxph(Surv(temp, surv) ~ pop + run + rep + motu + popf,
                   data = survdat) # don't interpret the second lines of information if the p-values of the first lines are not significant
 print(cox_reg2)
 summary(cox_reg2) # pay satisfactions data had 38 missing observations
 
 # what is the hazard ratio?
-# Harzard ratio = exp(-0.10971) = 0.8961
-# 1 - HR = 1 - 0.8961 =  0.1039 = the risk of an individual quitting their job will decrease by 10.4% 
-# for every additional dollar in hourly pay that the individual earned.
+# Harzard ratio = exp(-0.35287) = 0.70267
+# 1 - HR = 1 - 0.70267 =  0.29733 = the risk of an individual dying will decrease by 29.7% 
+# in motu G compared with motu A (which is 0).
 
-# reciprocal of HR = 1 / 0.8961 = 1.1160 = risk of staying in the organisation will increase 1.116 times 
-# for every additional dollar of hourly payed earned. If you do present the reciprocal, you should defintiely 
+# reciprocal of HR = 1 / 0.70267 = 1.423143 = the chance of an individual staying alive will increase 1.423143 times 
+# for motu G compared with motu A (which is based on 0). If you do present the reciprocal, you should defintiely 
 # report the HR because the confidence intervals are related to the HR and not the reciprocal.
 
 ## CHOOSE WHAT IS EASIEST TO EXPLAIN TO PEOPLE
-
-# write out the above in equation format
-# hypothetical values for a hispranic latino person who makes 16$ per hour and is satisfied with their pay
-RaceHL <- 1
-RaceW <- 0
-PayHour <- 16.00
-PaySat <- 4.00
-log_overallrisk <- .121*RaceHL -.044*RaceW -.110*PayHour + .141*PaySat
-print(log_overallrisk)
-exp(log_overallrisk) # the overall risk of quitting their job is 65.1% (1 - 0.3412978 = 0.6587022)
-                     # less likely to quit their job when compared to an individual with scores of zero
-                     # in each of the covariates in the model.
-
-# making covariates more meaningful
-# grand-mean centre continuous covariates
-survdat$c_Pay_hourly <- scale(survdat$Pay_hourly, center = T, scale = F) # now model will compare to the average not 0 which is meaningless
-survdat$c_Pay_sat <- scale(survdat$Pay_sat, center = T, scale = F)# now model will compare to the average not 0 which is meaningless
-
-# Change reference group to HispanicLatino for categorical covariate by reordering levels
-survdat$HL_Race <- factor(survdat$Race, levels = c("HispanicLatino", "Black", "White"))
-
-# Estimate Cox proportional hazard model with categorical & continuous covariates
-cox_reg3 <- coxph(Surv(LOS, censored) ~ HL_Race + c_Pay_hourly + c_Pay_sat,
-                  data = survdat)
-summary(cox_reg3)
-
-# hypothetical values for a hispranic latino person who makes 16$ per hour and is satisfied with their pay
-RaceB <- 0 # HL is now the reference group - they identify as HL, not W or B
-RaceW <- 0
-PayHour <- 16.00 - mean(survdat$Pay_hourly, na.rm = T) # drop people with missing data (na.rm)
-PayHour # this person has above average pay - the number is positive
-PaySat <- 4.00 - mean(survdat$Pay_sat, na.rm = T) # drop people with missing data (na.rm)
-PaySat # this person has above average pay satisfaction - the number is positive
-
-log_overallrisk <- -.121*RaceB -.165*RaceW -.110*PayHour + .141*PaySat
-print(log_overallrisk) # -0.1623714
-exp(log_overallrisk) # the overall risk of quitting their job is 15% (1 - 0.8501254 = 0.1498746)
-# less likely to quit their job when compared to an individual with scores of zero
-# in each of the covariates in the model (i.e., compared to hispanicLatinos with average hourly pay 
-# and have average levels of pay satisfaction).
 
 # Nested model comparisons (using anova)
 cox_reg1 # 701 individuals in the sample size
 cox_reg2 # sample is 663 (38 individuals removed)
 
-library(tidyr)
+anova(cox_reg1, cox_reg2) # cox_reg2 = model with motu is better than the fuller model with many covariates
 
-# estimate cox proportional hazards (ph) model (cox regression model)
-cox_reg1 <- coxph(Surv(LOS, censored) ~ Race,
-                  data = drop_na(survdat, LOS, censored, Race, Pay_hourly, Pay_sat)) # dont interpret the second lines of information if the p-values of the first lines are not significant
-summary(cox_reg1)
-# estimate cox proportional hazards (ph) model (cox regression model)
-cox_reg2 <- coxph(Surv(LOS, censored) ~ Race + Pay_hourly + Pay_sat,
-                  data = drop_na(survdat, LOS, censored, Race, Pay_hourly, Pay_sat)) # don't interpret the second lines of information if the p-values of the first lines are not significant
-summary(cox_reg2)
 
-anova(cox_reg1, cox_reg2) # cox_reg2 = full model = is better than the smaller nested model
+
